3e7a3802d2cfbd4f5228aea4031db898f320d30a
ElectricJack <jack.w.kern@gmail.com>
2011-09-26 20:46:28 -0700

    Added an example

---
+++ examples/SingleShifterExample/SingleShifterExample.ino
@@ -0,0 +1,19 @@
+#include <Serial2Parallel.h>
+
+Serial2Parallel shifter;
+
+void setup() {
+  shifter.init(8,9,10);
+}
+
+byte pin = 0;
+void loop() {
+  
+  shifter.clear();
+  shifter.set( pin, true );
+  shifter.sync();
+  
+  delay(100);
+  
+  pin = (++pin) % 8;
+}
